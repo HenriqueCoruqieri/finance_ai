c2635c70b61baf677951e3edaa49ecae0e7290e3
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-07-21 16:16:03 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./_components/ui/button"
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-white">Finance AI</h1>
+  return (
+    <div className="h-screen space-y-2 p-4 text-center">
+      <h1 className="text-bold text-3xl">Finance AI</h1>
+      <Button>Entrar</Button>
+    </div>
+  )
 }
