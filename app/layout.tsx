5c494b895e438b83d3da307ed7cc2acf6e64c6a0
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-07-22 15:00:46 -0300

    feat: update font to mulish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import { shadcn } from "@clerk/ui/themes"
 import type { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Mulish } from "next/font/google"
 import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const mulish = Mulish({
+  subsets: ["latin-ext"],
+  variable: "--font-sans",
 })
 
 export const metadata: Metadata = {
@@ -25,10 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
-    >
+    <html lang="en" className={`${mulish.className} dark h-full antialiased`}>
       <body className="flex min-h-full flex-col">
         <ClerkProvider
           afterSignOutUrl="/login"
