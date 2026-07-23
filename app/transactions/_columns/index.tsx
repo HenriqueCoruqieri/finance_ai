751643232e3fbcd1091d3ca9068d258e3af19345
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-07-23 00:51:37 -0300

    feat: ensure transaction table labels are rendered property

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -4,6 +4,30 @@ import { Transaction } from "@/app/generated/prisma/browser"
 import { ColumnDef } from "@tanstack/react-table"
 
 import TransactionTypeBadge from "../_components/type-badge"
+import { PenIcon, TrashIcon } from "lucide-react"
+import { Button } from "@/app/_components/ui/button"
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Comida",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+}
+
+export const TRANSACTIONS_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferência Bancária",
+  BAND_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  OTHER: "Outros",
+  PIX: "Pix",
+}
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -20,26 +44,54 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   },
 
   {
+    accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
 
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTIONS_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
 
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
 
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
 
   {
     accessorKey: "actions",
     header: "",
+    cell: () => {
+      return (
+        <div>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PenIcon />
+          </Button>
+        </div>
+      )
+    },
   },
 ]
