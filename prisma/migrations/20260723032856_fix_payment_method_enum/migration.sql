c036c6717d3881dcc7ea34d4228f76a9a63ace82
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-07-23 00:38:15 -0300

    fix: alter payment method field to BANK_SLIP

---
+++ prisma/migrations/20260723032856_fix_payment_method_enum/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [BANK_SPLIT] on the enum `PaymentMethod` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "PaymentMethod_new" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
+ALTER TABLE "Transaction" ALTER COLUMN "paymentMethod" TYPE "PaymentMethod_new" USING ("paymentMethod"::text::"PaymentMethod_new");
+ALTER TYPE "PaymentMethod" RENAME TO "PaymentMethod_old";
+ALTER TYPE "PaymentMethod_new" RENAME TO "PaymentMethod";
+DROP TYPE "public"."PaymentMethod_old";
+COMMIT;
